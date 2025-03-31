0eefd3bb7357e92b55f8223b9c0340c8059eeacb
Tshepiso28 <tshepisostudent@gmail.com>
2025-03-31 12:01:40 +0200

    Created a directive component to learn directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './home/home.component';
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
+import { DirectivesComponent } from './components/directives/directives.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent, DataBindingComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, DataBindingComponent, DirectivesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
